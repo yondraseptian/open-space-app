e248f5a4323da0ef6f942d76c70d6c14029212b8
yondraseptian <yondraseptian@gmail.com>
2024-05-12 15:29:05 +0700

    make test passed

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { IoEarthOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import LoginInput from '../components/LoginInput';
-import { useDispatch } from "react-redux";
-import {asyncSetAuthUser} from "../states/authUser/action.js";
+import { asyncSetAuthUser } from '../states/authUser/action.js';
 
 function LoginPage() {
   const dispatch = useDispatch(); // @TODO: get dispatch function from store
